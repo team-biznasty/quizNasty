e41064cb2f4b101a96082797cadba19b83239cde
Jim <jadavey@hotmail.com>
2021-03-23 15:45:39 +0000

    Sign Up js created

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const User = require('./models/user');
+const bodyParser = require('body-parser');
+const cors = require("cors");
 require("dotenv").config();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(cors());
 
 mongoose.connect(`mongodb+srv://${process.env.mongoUser}:${process.env.mongoPassword}@quiznasty01.xzhfa.mongodb.net/${process.env.mongoDB}?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
@@ -15,21 +21,20 @@ mongoose.connect(`mongodb+srv://${process.env.mongoUser}:${process.env.mongoPass
       console.log('MongoDB database connection established successfully!');
   })
 
-app.get('/', (req, res) => {
-    new User({name:"User",email:"user@email.com",password:"1234"}).save((err, obj) => {
-        if (err){
-            res.status(500).json({status:"Not Working", err});
-        } else {
-        res.status(200).json({status:"Working", obj});
-        }
-    });
-    
-});
+// app.get('/', (req, res) => {
+//     new User({name:"Use",email:"user@email.com",password:"1234"}).save((err, obj) => {
+//         if (err){
+//             res.status(500).json({status:"Not Working", err});
+//         } else {
+//         res.status(200).json({status:"Working", obj});
+//         }
+//     });
+// });
 
 app.use("/auth", require("./routes/userRouter"));
 app.use(express.json());
 
-app.listen(3000, () => {
-    console.log("Listening to port 3000");
+app.listen(5000, () => {
+    console.log("Listening to port 5000");
 });
 
