082d1bb0d555aa10ca13fd1033555ca678098317
Adam Manley <adam.p.manley@gmail.com>
2021-03-23 11:33:15 +0000

    basic quiz
    10 question quiz, calls api displays in terminal

---
+++ frontend/lib/quizGen.js
@@ -0,0 +1,12 @@
+const fetch = require("node-fetch");
+require("dotenv").config();
+
+const getQuiz = async() =>{
+    const url = `https://opentdb.com/api.php?amount=2`
+    let data = await fetch(url)
+    console.log(await data.json())
+    return await data.json()
+}
+getQuiz()
+module.exports=getQuiz
+
