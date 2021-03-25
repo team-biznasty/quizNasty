f6d5a3a87131bf60a40a813c6788d16ae932e863
Jim <jadavey@hotmail.com>
2021-03-25 15:10:53 +0000

    Showing All Answers
    Not shuffled and inside props.children of the array  ¯\_(ツ)_/¯ i

--- frontend/src/components/quizPage/QuizPage.js
+++ frontend/src/components/quizPage/QuizPage.js
@@ -6,8 +6,8 @@ const Quiz = () => {
   //fetch single game - just change the id - use insomnia or compass or game._id
   const [questions, setQuestions] = useState([]);
   const [count, setCount] = useState(0);
-  const [score, setScore] = useState(0);
-  const [answers, setAnswers] = useState([]);
+  // const [score, setScore] = useState(0);
+  // const [answers, setAnswers] = useState([]);
 
   useEffect(() => {
     const getter = async () => {
@@ -17,7 +17,7 @@ const Quiz = () => {
       const data = await response.json();
       console.log(data);
       setQuestions(data.results);
-      setAnswers(data.results)
+      // setAnswers(data.results)
     };
     getter();
   }, []);
@@ -25,34 +25,29 @@ const Quiz = () => {
   const questionComponents = questions.map((question, index) => {
     return <div key={index}>{question.question}</div>;
   });
-console.log(answers[0].correct_answer)
+
   const questionCount = questionComponents[count];
 
-/*const answerList = questions.map((answer, index) => {
-  return <div key={index}>{answer.correct_answer}</div>
-})*/
+  const answerList = questions.map((answer, index) => {
+    return (
+      <div key={index}>
+        {[...answer.incorrect_answers, answer.correct_answer]}
+      </div>
+    );
+  });
+  // const shuffle = (array) => {
+  //   let shuffledArray = array.sort(() => Math.random() - 0.5);
+  //   return shuffledArray;
+  // };
 
-/*for incorrect answers map over incorrect answerrs and slice them out as individula objects returns
-3 seperated objects from the Array
-
-then add the incorrect answers to answer list:
-add corecty answer to answer list 
-then shuffle
-*/
-
-// let answerList = () =>{
-//   questions.map((incAnswers, index) => {
-//       return((incAnswers.index.incorrect_answers));
-
-// })
-// }
-// console.log(answerList())
- // const answerCount = answerList[count];
+  console.log(answerList)
+  const answerCount = answerList[count];
 
   return (
     <div>
       {/* {questionComponents.length > 0 ? questionComponents : null} */}
       {questionCount}
+      {answerCount}
       <button className="submitButton" onClick={() => setCount(count + 1)}>
         Next Question
       </button>
