80ca74f6b4a0d8ee77582321c63d7fa13b688f60
Muge <alev.muge@gmail.com>
2021-03-30 16:29:37 +0100

    Random Quiz Page works with hard code

--- frontend/src/components/quizPage/QuizPage.js
+++ frontend/src/components/quizPage/QuizPage.js
@@ -1,11 +1,8 @@
-
 import React, { useState, useEffect } from "react";
-import Replacer from "../../lib/Replacer"
+import Replacer from "../../lib/Replacer";
 // import PostScore from "../postingScore/PostingScore";
-import QuizForm from "../../lib/quizGen"
+import QuizForm from "../../lib/quizGen";
 
-
-setTimeout(() => console.log("hiya"), 0);
 
 const Quiz = () => {
   //fetch single game - just change the id - use insomnia or compass or game._id
@@ -31,39 +28,51 @@ const Quiz = () => {
   }, []);
 
   const questionComponents = questions.map((question, index) => {
-    return <div key={question.id}>{Replacer(question.question)}</div>;
+    return (
+      <div key={question.id} className="current">
+        <h1>{Replacer(question.question)}</h1>
+      </div>
+    );
   });
 
- const questionCount = questionComponents[count];
-// const qcString = JSON.stringify(questionCount);
-// console.log(qcString)
-//let strungQC = Replacer(qcString)
-//console.log(strungQC)
+  const questionCount = questionComponents[count];
+  // const qcString = JSON.stringify(questionCount);
+  // console.log(qcString)
+  //let strungQC = Replacer(qcString)
+  //console.log(strungQC)
 
- console.log(questionCount)
+  console.log(questionCount);
   const answerList = questions.map((question, index) => {
     const answers = [question.correct_answer, ...question.incorrect_answers];
     function scoreIncrease() {
       setCount(count + 1);
 
-      setScore(score + 1)
+      setScore(score + 1);
     }
 
     answers.sort(() => Math.random() - 0.5);
     return (
-      <div key={question.id}>
+      <div key={question.id} className="answersWrapper">
         {answers.map((answer, idx) => {
           if (answer === question.correct_answer) {
             return (
-              <p className="correct_answer" key={idx}>
-                <button onClick={scoreIncrease}> {Replacer(answer)} </button>
-              </p>
+              <div className="correct_answer" key={idx}>
+                <button onClick={scoreIncrease} className="yellowBtn answerBtn">
+                  {" "}
+                  {Replacer(answer)}{" "}
+                </button>
+              </div>
             );
           } else {
             return (
-              <p className="incorrect_answer" key={idx}>
-                <button className="incorrect" onClick={() => setCount(count + 1)}>{Replacer(answer)}</button>
-              </p>
+              <div className="incorrect_answer" key={idx}>
+                <button
+                  className="incorrect yellowBtn answerBtn"
+                  onClick={() => setCount(count + 1)}
+                >
+                  {Replacer(answer)}
+                </button>
+              </div>
             );
           }
         })}
@@ -74,18 +83,16 @@ const Quiz = () => {
   console.log(answerList);
   const answerCount = answerList[count];
   return (
-
     <div>
-     { QuizForm }
+      {QuizForm}
       {/* {questionComponents.length > 0 ? questionComponents : null} */}
       {questionCount}
-      {answerCount}
-      {score}
-      {count}
+      <div>{answerCount}</div>
+      <div className="currentScore gameDetails"><h2>Total Score: {score}</h2></div>
+      <div className="currentQuestion gameDetails"><h2>Current Question #{count}</h2></div>
       {/* <PostScore difficulty={difficulty} category={category} amount={amount} score={score} /> */}
     </div>
-  )
-}
-
+  );
+};
 
 export default Quiz;
