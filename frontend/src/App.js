02c4f9eaa545937e01b59531f2000b6ec0b28851
Muge <alev.muge@gmail.com>
2021-03-30 12:53:41 +0100

    homepage and button routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,34 @@
 import "./App.css";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+
 //import SignUp from './components/signUp/SignUp'
 import Quiz from "./components/quizPage/QuizPage";
 
 import Navbar from "./components/navBar/Navbar";
 import Login from "./components/logIn/logIn";
 import Footer from "./components/footer/footer";
+import Homepage from "./components/homePage/homePage";
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <Navbar />
+      <Homepage />
+      <Switch>
       <div className="contentArea">
+      <Route exact path="/">
         <Quiz />
+        </Route>
+        <Route exact path="/login">
         <Login />
+        </Route>
       </div>
-
+      </Switch>
       <div className="footerArea">
         <Footer />
       </div>
+      </BrowserRouter>
     </div>
   );
 }
