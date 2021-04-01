bca4d680a0a1497f20fc503cc25b23f6cbdde984
Jim <jadavey@hotmail.com>
2021-04-01 11:48:13 +0100

    Posting Scores

--- frontend/src/components/leaderBoards/leaderBoards.js
+++ frontend/src/components/leaderBoards/leaderBoards.js
@@ -1,7 +1,29 @@
 import "./leaderboard.css";
+import axios from 'axios';
+import React, { useEffect, useState } from "react";
 
 const Leaderboard = () => {
+  const [Results, setResults] = useState([]);
+
+  const getScores = async () => {
+    await axios.post("http://localhost:5000/quiz").then((res) => {
+      setResults(res.data.results.flatMap((user) => {return user.results.map((result) => {
+        result.username = user.username;
+        return result
+    })}).sort((Results, result) => {return result.points - Results.points}));
+});
+};
+
+useEffect(() => {
+  if (Results.length === 0) {
+      getScores();
+  } else {
+      console.log(Results)
+  }
+})
+
   return (
+
     <div className="leaderboardContainer">
       <h1>Leaderboard</h1>
 
@@ -15,6 +37,7 @@ const Leaderboard = () => {
         </thead>
 
         <tbody>
+      
           <tr className="boardDetails">
             <td class="numberBoard">1</td>
             <td className="usernameBoard">User001</td>
--- models/QuizResults.js
+++ models/QuizResults.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 const QuizResults = new mongoose.Schema({
     name: {type: String, required:true},
-    difficulty: {type: String, required:true},
-    category: {type: String, required:true},
-    amount: {type: Number, required: true},
+    // difficulty: {type: String, required:true},
+    // category: {type: String, required:true},
+    // amount: {type: Number, required: true},
     score: {type: Number, required: true},
 });
 
