ac5819f1017bcf0833a6e0944400a183505c99ac
Muge <alev.muge@gmail.com>
2021-04-01 13:55:34 +0100

    Leaderboard Fetch data

--- frontend/src/components/leaderBoards/leaderBoards.js
+++ frontend/src/components/leaderBoards/leaderBoards.js
@@ -1,52 +1,92 @@
 import "./leaderboard.css";
-import axios from 'axios';
+import axios from "axios";
 import React, { useEffect, useState } from "react";
 
 const Leaderboard = () => {
-  const [Results, setResults] = useState([]);
+  const [results, setResults] = useState([]);
 
-  const getScores = async () => {
-    await axios.post("http://localhost:5000/quiz").then((res) => {
-      setResults(res.data.results.flatMap((user) => {return user.results.map((result) => {
-        result.username = user.username;
-        return result
-    })}).sort((Results, result) => {return result.points - Results.points}));
-});
-};
-
-useEffect(() => {
-  if (Results.length === 0) {
-      getScores();
-  } else {
-      console.log(Results)
-  }
-})
+  useEffect(() => {
+    const getter = async () => {
+      const response = await fetch(`http://localhost:5000/scores/quizresults`);
+      const data = await response.json();
+      console.log(data);
+      setResults(data);
+    };
+    getter();
+  }, []);
 
   return (
-
     <div className="leaderboardContainer">
       <h1>Leaderboard</h1>
 
       <table className="boardsArea">
         <thead className="boardTitles">
           <tr>
-            <th></th>
             <th>Users</th>
             <th>Score</th>
           </tr>
         </thead>
-
         <tbody>
-      
-          <tr className="boardDetails">
-            <td class="numberBoard">1</td>
-            <td className="usernameBoard">User001</td>
-            <td className="pointsBoard">100</td>
-          </tr>
+          {results.map((result, index) => {
+              return(
+            <tr className="boardDetails">
+              <td className="usernameBoard" key={result._id}>
+                {result.name}
+              </td>
+              <td className="pointsBoard" key={index}>
+                {result.score}
+              </td>
+            </tr>
+              )
+          })}
         </tbody>
       </table>
     </div>
   );
 };
-
 export default Leaderboard;
+
+/* const getScores = async () => {
+    await axios.post("http://localhost:5000/quiz").then((res) => {
+      setResults(
+        res.data.results
+          .flatMap((name) => {
+            return name.results.map((result) => {
+              return result;
+            });
+          })
+          .sort((Results, result) => {
+            return result.score - Results.score;
+          })
+      );
+    });
+  };
+ */
+/* useEffect(() => {
+    if (Results.length === 0) {
+      getScores();
+    } else {
+      console.log(Results);
+    }
+  }); */
+
+/* Results.filter((result) => {
+    return result;
+  }).map((result) => ( */
+
+/*     const id = results.id;
+
+    const getResults = () => {
+        axios.get("http://localhost:5000/quiz")
+            .then((response) =>{
+                const results = setResults(response.data);
+                console.log(response.data);
+            })
+            .catch((err) => {
+                console.log("error here", err);
+            });
+    }
+
+    useEffect (() => {
+        getResults();
+    }, []); */
